732f1ce6356c9e706fd71a3cc0017b505c63a8a3
CodenificienT <codenificient@tutanota.com>
2021-09-12 21:22:06 -0400

    Initial commit

---
+++ Gobook/constants/constant.go
@@ -0,0 +1,8 @@
+package constants
+
+import "fmt"
+
+func constants()  {
+	const greet string = "Hola mi amor!"
+	fmt.Println(greet)
+}
---
+++ Intro/Gholla.go
@@ -0,0 +1,7 @@
+package gholla
+
+import "fmt"
+
+func sayHello() {
+	fmt.Println("Hola a mi Ghola!")
+}
