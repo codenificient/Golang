79debe0591f079ca49df68651bc0a94770e9f537
CodenificienT <61930093+codenificient@users.noreply.github.com>
2021-12-16 12:29:52 -0500

    add new folders

--- GoByExamples/Values.go
+++ GoByExamples/Values.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(true && true)
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1=", 1+1)
+
+	fmt.Println("7.0+3.7=", 7.3+3.7)
+
 	fmt.Println(true && false)
-	fmt.Println(true || true)
 	fmt.Println(true || false)
-	fmt.Println(false || false)
 	fmt.Println(!true)
 }
